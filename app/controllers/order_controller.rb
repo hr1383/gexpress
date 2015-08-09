6a56428e152ed58188e1222a1ac420c46f097f10
Harsh <hrsht.rastogi13@gmail.com>
2015-08-09 16:12:35 -0700

    adding call for order and fixing the html

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,36 @@
+class OrderController < ApplicationController
+
+	def create
+		shipping_address = params['shipping_address']
+		billing =  params['billing']
+		sku = params['sku']
+		order = Order.new(:lname => shipping_address['lname'],
+						  :fname => shipping_address['fname'],
+						  :sku_model => sku['device'],
+						  :sku_size => sku['size'],
+						  :sku_memory => sku['memory'],
+						  :sku_state => sku['state'],
+						  :sku_carrier => sku['carrier'],
+						  :payment_method => billing['payment_method'],
+						  :shipping_addr => shipping_address['addr'],
+						  :shipping_addr1 => shipping_address['addr1'],
+						  :shipping_city => shipping_address['city'],
+						  :shipping_state => shipping_address['state'],
+						  :shipping_zipcode => shipping_address['zipcode'],
+						  :shipping_phone => shipping_address['phone'],
+						  :shipping_email => shipping_address['email'],
+						  :billing_addr => billing['addr'],
+						  :billing_addr1 => billing['addr1'],
+						  :billing_city => billing['city'],
+						  :billing_state => billing['state'],
+						  :billing_zipcode => billing['zipcode'],
+						  :billing_phone => billing['phone'],
+						  :billing_email => billing['email'],
+						  :billing_paypal_email => billing['paypalemail'],
+						  :order_status => 'NEW')
+		order.save
+		respond_to do |format|    	
+    		format.json  { render :json => '' }
+		end
+	end
+end
---
+++ app/models/order.rb
@@ -0,0 +1,38 @@
+class Order
+  include Mongoid::Document
+  field  :order_id
+  field :fname
+  field :lname
+  field :sku_model
+  field :sku_size
+  field :sku_memory
+  field :sku_state
+  field :sku_carrier
+  field :payment_method
+  field :shipping_addr
+  field :shipping_addr1
+  field :shipping_city
+  field :shipping_state
+  field :shipping_zipcode
+  field :shipping_email
+  field :shipping_phone
+  field :billing_addr
+  field :billing_addr1
+  field :billing_city
+  field :billing_state
+  field :billing_zipcode
+  field :paypal_email
+  field :order_status
+  field :created_at, :type => Date
+  field :shipped_on, :type => Date
+  field :received_on, :type => Date
+
+  after_initialize :add_orderid
+  def add_orderid
+  	puts "hell"
+  	self.order_id = loop do
+  		token = SecureRandom.urlsafe_base64
+  		break token unless Order.where(order_id: token).count != 0
+	end
+  end
+end
