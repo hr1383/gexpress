52a6dd64c639f95f96abbcd5abf9cd9dc1bbf50f
Harsh <hrsht.rastogi13@gmail.com>
2015-10-01 00:12:06 -0700

    use ENV variables

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,15 +1,12 @@
 ActionMailer::Base.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 587,
-    :user_name =>  'hrsht.rastogi13@gmail.com',
-    # ENV['MANDRILL_USERNAME'],
-    :password  => 'NlUT51_5lNYpsZub9IHLhw',
-    # ENV['MANDRILL_API_KEY'],
+    :user_name => ENV['MANDRILL_USERNAME'],
+    :password  => ENV['MANDRILL_API_KEY'],
     :domain    => 'heroku.com'
   }
 ActionMailer::Base.delivery_method = :smtp
 
 MandrillMailer.configure do |config|
-  config.api_key = 'NlUT51_5lNYpsZub9IHLhw'
-  # ENV['MANDRILL_API_KEY']
+  config.api_key = ENV['MANDRILL_API_KEY']
 end
