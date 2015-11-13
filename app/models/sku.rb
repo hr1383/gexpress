911cffbc0dbf0c7e1b3d1a03c60c83de9acab1fb
Harsh <hrsht.rastogi13@gmail.com>
2015-11-12 22:30:18 -0800

    fix the sku mode

--- app/models/sku.rb
+++ app/models/sku.rb
@@ -2,10 +2,7 @@ class Sku
   include Mongoid::Document
   field :device
   field :carrier
-  field :lock
+  field :price, :type => Integer
   field :size, :type => Integer
-  field :good, :type => Integer
-  field :bad, :type => Integer
-  field :flawless, :type => Integer
-  field :broken, :type => Integer
+  field :state
 end
