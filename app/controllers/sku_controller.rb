07bb81b3dd4762475c8aa9f249785ee6eb400115
Harsh <hrsht.rastogi13@gmail.com>
2015-11-12 21:26:15 -0800

    added some log

--- app/controllers/sku_controller.rb
+++ app/controllers/sku_controller.rb
@@ -8,6 +8,7 @@ class SkuController < ApplicationController
   	 sku = Sku.collection.find({'device' =>params['device'], 'carrier'=>params['carrier'], 'size' => params['size'].to_i,
   	  'state'=>params['state']}).first;
   	end
+  	puts sku.count
   	 respond_to do |format|    	
     	format.json  { render :json => sku } # don't do msg.to_json
   end
